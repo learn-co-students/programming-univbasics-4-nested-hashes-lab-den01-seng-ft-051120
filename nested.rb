eb54683fe139dca3157ba1fb45f986c1448e4b37
jdbuysse <jdbuysse@gmail.com>
2020-04-20 12:28:50 -0600

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+  programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -82,6 +84,14 @@ def adding_matz
       }
     }
 
+  programmer_hash = {
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+  }
+  programmer_hash
+
 end
 
 def changing_alan
@@ -103,6 +113,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash
 
 end
 
@@ -125,5 +138,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+  programmer_hash[:dennis_ritchie][:languages].append("Assembly")
+
+  programmer_hash
 
 end
